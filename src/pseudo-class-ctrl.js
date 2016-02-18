27a4e1b0af64cdae2d32893d976fd715cddc7c12
Nicolas Coden <nicos.coden@neuf.fr>
2016-02-18 12:24:16 +0100

    Fix huge naming mistake
    You're allowed to slap me for this horrible and ridiculous mistake !

--- src/pseudo-class-ctrl.js
+++ src/pseudo-class-ctrl.js
@@ -1,25 +1,17 @@
-var events = {
-  active: ['mousedown', 'mouseup'],
-  hover: ['mouseenter', 'mouseleave']
-};
-
-function PseudoClassCtrlLink(scope, elem, attrs) {
+function PseudoClassCtrlLink(scope, element, attrs) {
   var pseudoClasses = attrs['pseudoClassCtrl'] || 'active hover';
   pseudoClasses = pseudoClasses.split(' ');
 
   for (var i = 0; i < pseudoClasses.length; i++) {
     (function(pseudoClass) {
-
-      elem.on(events[pseudoClass][0], function(e) {
-        e.stopPropagation();
-        elem.parent().trigger(events[pseudoClass][1]);
-      });
-
-      elem.on(events[pseudoClass][1], function(e) {
-        e.stopPropagation();
-        elem.closest(':' + pseudoClass).trigger(events[pseudoClass][0]);
-      });
-
+      element.onPseudoClass(pseudoClass,
+        function(e) {
+          element.addClass('pseudo-class--' + pseudoClass);
+        },
+        function(e) {
+          element.removeClass('pseudo-class--' + pseudoClass);
+        }
+      )
     })(pseudoClasses[i]);
   }
 }
--- src/pseudo-class-scope.js
+++ src/pseudo-class-scope.js
@@ -1,17 +1,25 @@
-function PseudoClassScopeLink(scope, element, attrs) {
+var events = {
+  active: ['mousedown', 'mouseup'],
+  hover: ['mouseenter', 'mouseleave']
+};
+
+function PseudoClassScopeLink(scope, elem, attrs) {
   var pseudoClasses = attrs['pseudoClassScope'] || 'active hover';
   pseudoClasses = pseudoClasses.split(' ');
 
   for (var i = 0; i < pseudoClasses.length; i++) {
     (function(pseudoClass) {
-      element.onPseudoClass(pseudoClass,
-        function(e) {
-          element.addClass('pseudo-class--' + pseudoClass);
-        },
-        function(e) {
-          element.removeClass('pseudo-class--' + pseudoClass);
-        }
-      )
+
+      elem.on(events[pseudoClass][0], function(e) {
+        e.stopPropagation();
+        elem.parent().trigger(events[pseudoClass][1]);
+      });
+
+      elem.on(events[pseudoClass][1], function(e) {
+        e.stopPropagation();
+        elem.closest(':' + pseudoClass).trigger(events[pseudoClass][0]);
+      });
+
     })(pseudoClasses[i]);
   }
 }
