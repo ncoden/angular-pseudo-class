f597c230c789cf1f60837d299b3b8f1d09f3836a
Nicolas Coden <nicos.coden@neuf.fr>
2016-02-18 12:18:13 +0100

    Fix angular export

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import jQuery from 'jquery';
 import jQueryPseudoClass from 'jquery-pseudo-class';
 
-import {pseudoClassCtrlDirective} from './pseudo-class-ctrl.js';
-import {pseudoClassScopeDirective} from './pseudo-class-scope.js';
+import {PseudoClassCtrlDirective} from './pseudo-class-ctrl.js';
+import {PseudoClassScopeDirective} from './pseudo-class-scope.js';
 
-let app = angular.module('angularPseudoClass')
-    .directive('pseudoClassCtrl', pseudoClassCtrlDirective)
-    .directive('pseudoClassScope', pseudoClassScopeDirective)
+angular.module('angularPseudoClass', [])
+    .directive('pseudoClassCtrl', PseudoClassCtrlDirective)
+    .directive('pseudoClassScope', PseudoClassScopeDirective)
     ;
 
-export default app;
+export default 'angularPseudoClass';
